f0536c18e6f4d1bec7b9d6bc43381e4fa7fd4aed
duydang2002 <duydang312@gmail.com>
2023-12-10 22:05:50 +0700

    NPC Update

--- Assets/Script/NPC_Controller.cs
+++ Assets/Script/NPC_Controller.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
 
@@ -8,11 +9,13 @@ public class NPC_Controller : Interactable
 
     private BoxCollider2D boxCollider;
     Transform player;
+    Animator animatorMove;
     // Start is called before the first frame update
     void Start()
     {
         player = GameManager.instance.player.transform;
         boxCollider = GetComponent<BoxCollider2D>();
+        animatorMove = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,6 +25,18 @@ public class NPC_Controller : Interactable
     }
     public override void Interact(Character character)
     {
+        float horizontal = transform.position.y - player.position.y;
+        float vertical = transform.position.x - player.position.x;
         
+        Debug.Log(horizontal + " " + vertical);
+        animatorMove.SetFloat("Horizontal", -horizontal);
+        animatorMove.SetFloat("Vertical", -vertical);
+
+ /*       float x = transform.position.x - player.position.x;
+        float y = transform.position.y - player.position.y;
+        if (x <0)
+        {
+
+        }*/
     }
 }
