a1b5c869cdd49ea073a707fab78b10a1d84d80df
Lê Kỳ Anh <anh.lk207586@sis.hust.edu.vn>
2023-12-19 23:35:32 +0700

    Store

---
+++ Assets/ShopScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopScript : MonoBehaviour
+{
+
+    private Transform Container;
+    private Transform ShopItemTemplate;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Container = transform.Find("Container");
+        ShopItemTemplate = Container.Find("ShopItemTemplate");
+        ShopItemTemplate.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Transtion.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transtion : MonoBehaviour
+{
+    public string sceneName;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+ 
+}
+    
