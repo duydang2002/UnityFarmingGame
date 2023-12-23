b448862cc871da0dced0f0d7e61106243f90bc16
nguytvananh <anh.NTV200035@sis.hust.edu.vn>
2023-12-23 16:27:12 +0700

    add plowing + seeding

--- Assets/Script/CharacterToolsController.cs
+++ Assets/Script/CharacterToolsController.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using Unity.Burst.CompilerServices;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
+using UnityEngine.Tilemaps;
 
-
-    public class CharacterToolsController : MonoBehaviour
+public class CharacterToolsController : MonoBehaviour
     {
         MainCharacterControl characterController;
         Rigidbody2D rgbd2d;
@@ -15,50 +15,89 @@ using UnityEngine.TextCore.Text;
         [SerializeField] private LayerMask playerMask;
         [SerializeField] MarkerManager markerManager;
         [SerializeField] TileMapReadController tileMapReadController;
+        [SerializeField] float maxDistance = 2f;
+        [SerializeField] CropsManager cropsManager;
+        [SerializeField] TileData plowableTiles;
+
+        Vector3Int selectedTilePosition;
+        bool selectable;
 
         private void Awake()
         {
         characterController = GetComponent<MainCharacterControl>();
             rgbd2d = GetComponent<Rigidbody2D>();
         }
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
+        
         void Update()
         {
+            SelectTile();
+            CanSelectCheck();
             Marker();
             if (Input.GetMouseButtonDown(0))
             {
-                UseTool();
+                if (UseToolWorld() == true)
+                {
+                    return;
+                }
+                UseToolGrid();
             }
+        }
+
+        private void SelectTile()
+        {
+            selectedTilePosition = tileMapReadController.GetGridPosition(Input.mousePosition, true);
+        }
+
+        void CanSelectCheck()
+        {
+            Vector2 characterPosition = transform.position;
+            Vector2 cameraPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            selectable = Vector2.Distance(characterPosition, cameraPosition) < maxDistance;
+            markerManager.Show(selectable);
         }
 
         private void Marker()
         {
-            Vector3Int gridPosition = tileMapReadController.GetGridPosition(Input.mousePosition, true);
-            markerManager.markedCellPosition = gridPosition;
+            //Vector3Int gridPosition = tileMapReadController.GetGridPosition(Input.mousePosition, true);
+            markerManager.markedCellPosition = selectedTilePosition;
 
         }
-        private void UseTool()
+
+        private bool UseToolWorld()
         {
         Vector2 position = rgbd2d.position + characterController.lastMotionVector*offsetDistance;
             
             Collider2D[] collider2s = Physics2D.OverlapCircleAll(position, sizeOfInteractableArea);
             
             foreach (Collider2D c in collider2s)
-        {
-            ToolHit hit = c.GetComponent<ToolHit>();
-            
-            if (hit != null)
             {
-                hit.Hit(character);
-                break;
+                ToolHit hit = c.GetComponent<ToolHit>();
+
+                if (hit != null)
+                {
+                    hit.Hit(character);
+                    return true;
+                }
             }
-        }
+            return false;
         }
         
+        private void UseToolGrid()
+        {
+        if (selectable == true)
+            {
+                TileBase tileBase = tileMapReadController.GetTileBase(selectedTilePosition);
+                TileData tileData = tileMapReadController.GetTileData(tileBase);
+
+                if (tileData != plowableTiles)
+                {
+                    return;
+                }
+                if (cropsManager.Check(selectedTilePosition))
+                {
+                    cropsManager.Seed(selectedTilePosition);
+                }
+                cropsManager.Plow(selectedTilePosition);
+            }
+        }
     }
--- Assets/Script/MarkerManager.cs
+++ Assets/Script/MarkerManager.cs
@@ -9,15 +9,22 @@ public class MarkerManager : MonoBehaviour
     [SerializeField] TileBase tile;
     public Vector3Int markedCellPosition;
     Vector3Int oldCellPosition;
-    //bool show;
+    bool show;
 
     private void Update()
     {
-        
+        if (show == false)
+        {
+            return;
+        }
         targetTilemap.SetTile(oldCellPosition, null);
         targetTilemap.SetTile(markedCellPosition, tile);
         oldCellPosition = markedCellPosition;
     }
 
-    
+    internal void Show(bool selectable)
+    {
+        show = selectable;
+        targetTilemap.gameObject.SetActive(show);
+    }
 }
