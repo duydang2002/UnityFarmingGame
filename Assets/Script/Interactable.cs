af99b80b8d848b36c5e947a90c64d8ddd895a5ad
btdun2002 <bui123456trongdung@gmail.com>
2024-02-04 01:06:11 +0700

    Add comment

--- Assets/Script/Interactable.cs
+++ Assets/Script/Interactable.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
 
+// This class can be overrided by sub-classes of Interactable to
+// process different interact activities between characters and 
+// objects in the game
 public class Interactable : MonoBehaviour
 {
-  public virtual void Interact( Character character)
-    {
+  public virtual void Interact(Character character)
+  {
 
-    }
+  }
 }
